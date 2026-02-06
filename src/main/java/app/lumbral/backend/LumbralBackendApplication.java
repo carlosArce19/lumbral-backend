6d969169c8953643e594877014cddda48ebf306a
carlosArce19 <k-rlos9403@hotmail.com>
2026-02-05 21:28:59 -0500

    chore: initial Spring Boot scaffold (Java 21, Maven, YAML)

---
+++ src/main/java/app/lumbral/backend/LumbralBackendApplication.java
@@ -0,0 +1,13 @@
+package app.lumbral.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumbralBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LumbralBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/lumbral/backend/LumbralBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package app.lumbral.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumbralBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
